89988564c7ffadf5170cde5058e8f2265fa5fe16
Armando Genis <83677824+armando-genis@users.noreply.github.com>
2023-08-17 12:07:56 -0600

    odom and cmd_vel updated

--- regulated_pure_pursuit/src/pure_pursuit.cpp
+++ regulated_pure_pursuit/src/pure_pursuit.cpp
@@ -15,18 +15,26 @@ public:
 
         this->declare_parameter("x", std::vector<double>{});
         this->declare_parameter("y", std::vector<double>{});
+        this->declare_parameter("min_lookahead", 0.0);
+        this->declare_parameter("max_lookahead", 0.0);
+        this->declare_parameter("lookahead_ratio", 0.0);
+
+
+
 
         this->get_parameter("x", x_waypoints_);
         this->get_parameter("y", y_waypoints_);
+        this->get_parameter("min_lookahead", min_lookahead_);
+        this->get_parameter("max_lookahead", max_lookahead_);
+        this->get_parameter("lookahead_ratio", lookahead_ratio_);
+
 
         // Subscriber to Odometry message
         subscription_ = this->create_subscription<nav_msgs::msg::Odometry>("/racecar/odom_racecar", 10, std::bind(&PurePursuit::odom_callback, this, std::placeholders::_1));
 
         // Publisher for nearest waypoint and the next waypoint
-        marker_publisher_ = this->create_publisher<visualization_msgs::msg::Marker>("nearest_waypoint_marker", 10);
         next_marker_publisher_ = this->create_publisher<visualization_msgs::msg::Marker>("next_waypoint_marker", 10);
 
-        
         // Publisher for "cmd"
         cmd_vel_publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/racecar/cmd_racecar", 10);
         
@@ -36,8 +44,6 @@ public:
     }
 
 private:
-
-
     void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg) {
         current_pose_x_ = msg->pose.pose.position.x;
         current_pose_y_ = msg->pose.pose.position.y;
@@ -45,52 +51,31 @@ private:
 
     void pub_callback() {
 
-        int nearest_point_index = find_nearest_point(current_pose_x_, current_pose_y_);
-        int next_point_index = nearest_point_index + 1;
+        int next_point_index = find_nearest_point(current_pose_x_, current_pose_y_);
 
-        if (nearest_point_index != -1) {
-            visualization_msgs::msg::Marker marker;
-            marker.header.stamp = this->now();
-            marker.header.frame_id = "odom_demo";
-            marker.type = visualization_msgs::msg::Marker::SPHERE;
-            marker.action = visualization_msgs::msg::Marker::ADD;
-            marker.pose.position.x = x_waypoints_[nearest_point_index];
-            marker.pose.position.y = y_waypoints_[nearest_point_index];
-            marker.pose.position.z = 0;
-            marker.scale.x = 0.2; // Adjust as needed
-            marker.scale.y = 0.2; // Adjust as needed
-            marker.scale.z = 0.2; // Adjust as needed
-            marker.color.a = 1.0; // Must be non-zero
-            marker.color.r = 1.0; // Red color
-            marker.color.g = 0.0;
-            marker.color.b = 0.0;
+
+        if (next_point_index != -1) {
+
+            // Marker for next_point_index
+            visualization_msgs::msg::Marker next_marker;
+            next_marker.header.stamp = this->now();
+            next_marker.header.frame_id = "odom_demo";
+            next_marker.type = visualization_msgs::msg::Marker::SPHERE;
+            next_marker.action = visualization_msgs::msg::Marker::ADD;
+            next_marker.pose.position.x = x_waypoints_[next_point_index];
+            next_marker.pose.position.y = y_waypoints_[next_point_index];
+            next_marker.scale.x = 0.2;
+            next_marker.scale.y = 0.4;
+            next_marker.scale.z = 0.4;
+            next_marker.color.a = 1.0;
+            next_marker.color.r = 1.0;   // blue color for differentiation
+            next_marker.color.g = 0.0;
+            next_marker.color.b = 0.0;
             
-            marker_publisher_->publish(marker);
-
-            // Check if next_point_index is valid
-            if (next_point_index < static_cast<int>(x_waypoints_.size())) {
-                // Marker for next_point_index
-                visualization_msgs::msg::Marker next_marker;
-                next_marker.header.stamp = this->now();
-                next_marker.header.frame_id = "odom_demo";
-                next_marker.type = visualization_msgs::msg::Marker::SPHERE;
-                next_marker.action = visualization_msgs::msg::Marker::ADD;
-                next_marker.pose.position.x = x_waypoints_[next_point_index];
-                next_marker.pose.position.y = y_waypoints_[next_point_index];
-                next_marker.scale.x = 0.2;
-                next_marker.scale.y = 0.2;
-                next_marker.scale.z = 0.2;
-                next_marker.color.a = 1.0;
-                next_marker.color.r = 0.0;   // blue color for differentiation
-                next_marker.color.g = 0.0;
-                next_marker.color.b = 1.0;
-                
-                next_marker_publisher_->publish(next_marker);
-            }
+            next_marker_publisher_->publish(next_marker);
 
         }
     }
-
 
     int find_nearest_point(double curr_x, double curr_y) {
         std::vector<double> ranges;
@@ -100,12 +85,20 @@ private:
             double eucl_d = std::sqrt(eucl_x + eucl_y);
             ranges.push_back(eucl_d);
         }
+        
         auto min_it = std::min_element(ranges.begin(), ranges.end());
-        return std::distance(ranges.begin(), min_it);
+        size_t closest_index = std::distance(ranges.begin(), min_it);
+
+        // Here's the condition implemented in C++
+        if (closest_index + 1 < x_waypoints_.size()) {
+            return closest_index + 1;
+        } else {
+            return x_waypoints_.size() - 1;
+        }
     }
 
+
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_;
-    rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_publisher_;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_publisher_;
     rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr next_marker_publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
@@ -113,8 +106,9 @@ private:
     std::vector<double> y_waypoints_;
     double current_pose_x_;
     double current_pose_y_;
-
-
+    double min_lookahead_;
+    double max_lookahead_;
+    double lookahead_ratio_;
 };
 
 int main(int argc, char** argv) {
