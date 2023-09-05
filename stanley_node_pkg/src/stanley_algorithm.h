775c17b0f6f66927075ad07a33a0077d538a0cd5
Armando Genis <83677824+armando-genis@users.noreply.github.com>
2023-09-04 21:51:55 -0600

    stanly started

--- stanley_node_pkg/src/stanley_algorithm.h
+++ stanley_node_pkg/src/stanley_algorithm.h
@@ -19,38 +19,17 @@ double lateralController(const std::vector<Eigen::VectorXd>& waypoints, double x
     // Heading error
     double yaw_path = std::atan2(waypoints.back()[1]-waypoints.front()[1], waypoints.back()[0]-waypoints.front()[0]);
     double yaw_diff_heading = yaw_path - yaw;
-    if (yaw_diff_heading > M_PI) yaw_diff_heading -= 2 * M_PI;
-    if (yaw_diff_heading < -M_PI) yaw_diff_heading += 2 * M_PI;
 
-    // Crosstrack error
+
     double crosstrack_error = std::numeric_limits<double>::max();
     for (const auto& waypoint : waypoints) {
         double dist = std::pow(x - waypoint[0], 2) + std::pow(y - waypoint[1], 2);
         crosstrack_error = std::min(crosstrack_error, dist);
     }
-    crosstrack_error = std::sqrt(crosstrack_error);
-    double yaw_cross_track = std::atan2(y - waypoints.front()[1], x - waypoints.front()[0]);
-    double yaw_path2ct = yaw_path - yaw_cross_track;
-    if (yaw_path2ct > M_PI) yaw_path2ct -= 2 * M_PI;
-    if (yaw_path2ct < -M_PI) yaw_path2ct += 2 * M_PI;
-    if (yaw_path2ct > 0) crosstrack_error = std::abs(crosstrack_error);
-    else crosstrack_error = -std::abs(crosstrack_error);
-    double k_e = 0.3;
-    double yaw_diff_crosstrack = std::atan(k_e * crosstrack_error / v);
 
-    // Final expected steering
-    double steer_expect = yaw_diff_crosstrack + yaw_diff_heading;
-    if (steer_expect > M_PI) steer_expect -= 2 * M_PI;
-    if (steer_expect < -M_PI) steer_expect += 2 * M_PI;
 
-    steer_expect = std::min(1.22, steer_expect);
-    steer_expect = std::max(-1.22, steer_expect);
-
-    // Update
     
-    steer_output = setSteer(steer_expect);
-    
-    return yaw_diff_heading;
+    return crosstrack_error;
 }
 
 double setSteer(double input_steer_in_rad) {
@@ -67,3 +46,35 @@ double setSteer(double input_steer_in_rad) {
 
 
 #endif
+
+
+
+    // // Crosstrack error
+    // double crosstrack_error = std::numeric_limits<double>::max();
+    // for (const auto& waypoint : waypoints) {
+    //     double dist = std::pow(x - waypoint[0], 2) + std::pow(y - waypoint[1], 2);
+    //     crosstrack_error = std::min(crosstrack_error, dist);
+    // }
+
+    
+    // crosstrack_error = std::sqrt(crosstrack_error);
+    // double yaw_cross_track = std::atan2(y - waypoints.front()[1], x - waypoints.front()[0]);
+    // double yaw_path2ct = yaw_path - yaw_cross_track;
+    // if (yaw_path2ct > M_PI) yaw_path2ct -= 2 * M_PI;
+    // if (yaw_path2ct < -M_PI) yaw_path2ct += 2 * M_PI;
+    // if (yaw_path2ct > 0) crosstrack_error = std::abs(crosstrack_error);
+    // else crosstrack_error = -std::abs(crosstrack_error);
+    // double k_e = 0.3;
+    // double yaw_diff_crosstrack = std::atan(k_e * crosstrack_error / v);
+
+    // // Final expected steering
+    // double steer_expect = yaw_diff_crosstrack + yaw_diff_heading;
+    // if (steer_expect > M_PI) steer_expect -= 2 * M_PI;
+    // if (steer_expect < -M_PI) steer_expect += 2 * M_PI;
+
+    // steer_expect = std::min(1.22, steer_expect);
+    // steer_expect = std::max(-1.22, steer_expect);
+
+    // // Update
+    
+    // steer_output = setSteer(steer_expect);
