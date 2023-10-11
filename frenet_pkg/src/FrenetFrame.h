ab2a03635442871e3f206bd62f8a34b2311c828d
Armando Genis <a01654262@itesm.mx>
2023-10-11 12:51:42 -0600

    frenet added

---
+++ frenet_pkg/src/FrenetFrame.h
@@ -0,0 +1,23 @@
+// C++
+#include <iostream>
+#include <vector>
+#include <Eigen/Dense>
+
+using namespace std;
+
+class FrenetFrame
+{
+private:
+    /* data */
+public:
+    FrenetFrame(/* args */);
+    ~FrenetFrame();
+};
+
+FrenetFrame::FrenetFrame(/* args */)
+{
+}
+
+FrenetFrame::~FrenetFrame()
+{
+}
